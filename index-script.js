3bc741aa7b00792a791482dbd4f6748ee9af4e06
Merav <merav.mi@gmail.com>
2019-05-17 22:12:07 +0300

    added slideshow of changing images+text in mid bottom row

--- index-script.js
+++ index-script.js
@@ -1,3 +1,4 @@
+
 var slideIndex = 0;
 carousel();
 
@@ -10,5 +11,5 @@ function carousel() {
     slideIndex++;
     if (slideIndex > x.length) { slideIndex = 1 }
     x[slideIndex - 1].style.display = "block";
-    setTimeout(carousel, 2000); // Change image every 2 seconds
+    setTimeout(carousel, 5000); // Change image every 5 seconds
 }
