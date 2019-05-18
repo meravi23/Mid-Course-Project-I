e1db0b9d156613da1b3bdfd82f242e936bfaa1dc
Merav <merav.mi@gmail.com>
2019-05-18 15:58:24 +0300

    last adjustments

--- contact-script.js
+++ contact-script.js
@@ -1,5 +1,22 @@
-function displayError() {
-    // alert("hi");
-    document.getElementsByClassName("rqrd").style.display = "initial";
+// function validateForm() {
+//     // alert("hi");
+//     var x = document.getElementById("fname").value;
+//     // alert(x);
+
     
-}
+
+//     if (x == "") {
+//         // alert("required");
+//         document.getElementById("rq-fname").style.display = "block";
+//     }
+// }
+
+// var eml = document.getElementById("email");
+
+// eml.addEventListener("keyup", function(event) {
+//     if (eml.validity.typeMismatch) {
+//         eml.setCustomValidity("קלט לא מתאים");
+//     } else {
+//         eml.setCustomValidity("");
+//     }
+// });
